09f92f0f623549731af01e7453f72261bc721cd5
OmarElgammal1 <omarelgammal2004@gmail.com>
2023-09-27 13:53:45 +0300

    Minimax draw score fix

--- TicTacToeAI/game.py
+++ TicTacToeAI/game.py
@@ -2,17 +2,17 @@ class Game:
     size = 3
     n_turns = 0
     board = []
-    def __init__(self, size):
-        self.size = size
-        self.board = [[' ' for i in range(size)] for j in range(size)]
+    def __init__(self):
+        self.board = [[' ' for i in range(self.size)] for j in range(self.size)]
 
-    
+
     def display(self):
         print(f" {self.board[0][0]} | {self.board[0][1]} | {self.board[0][2]} ")
         print("---|---|---")
         print(f" {self.board[1][0]} | {self.board[1][1]} | {self.board[1][2]} ")
         print("---|---|---")
         print(f" {self.board[2][0]} | {self.board[2][1]} | {self.board[2][2]} ")
+
 
     def make_move(self, x, y):
         if(self.board[x][y] == ' '):
@@ -22,31 +22,29 @@ class Game:
                 self.board[x][y] = 'O'
             self.n_turns += 1
             return True
-            
-        
+
         return False
-    
+
+
     def undo(self, x, y):
         self.board[x][y] = ' '
         self.n_turns -= 1
 
-    def check_win(self):
 
+    def check_win(self):
         for i in range(self.size):
             if(self.board[i][0] == self.board[i][1] and self.board[i][1] == self.board[i][2] and self.board[i][0] != ' '):
-                return True
+                return self.board[i][0]
         
             if(self.board[0][i] == self.board[1][i] and self.board[1][i] == self.board[2][i] and self.board[0][i] != ' '):
-                return True
+                return self.board[0][i]
         
         if(self.board[0][0] == self.board[1][1] and self.board[1][1] == self.board[2][2] and self.board[0][0] != ' '):
-            return True
+            return self.board[0][0]
 
         if(self.board[0][2] == self.board[1][1] and self.board[1][1] == self.board[2][0] and self.board[0][2] != ' '):
-            return True
-
-        
-        return 0
+            return self.board[0][2]
+        return False
 
 def bestMove(g):
     best_score = -1000
@@ -55,7 +53,7 @@ def bestMove(g):
     for i in range(g.size):
         for j in range(g.size):
             if(g.make_move(i, j)):
-                score = minimax(g, True)
+                score = minimax(g, False)
                 g.undo(i, j)
                 if(score > best_score):
                     best_score = score
@@ -66,11 +64,14 @@ def bestMove(g):
 
 
 def minimax(g, is_maximizing):
-    if(g.check_win() and g.n_turns % 2 == 1):
+    if(g.check_win() == 'X'):
         return -10
-    
-    elif(g.check_win() and g.n_turns % 2 == 0):
+
+    elif(g.check_win() == 'O'):
         return 10
+
+    elif(g.n_turns == 9):
+        return 0
     
     if(is_maximizing):
         best_score = -1000
@@ -93,14 +94,14 @@ def minimax(g, is_maximizing):
     return best_score
 
 def play():
-    g = Game(3)
+    g = Game()
     while(True):
         g.display()
         print("Enter your move: ")
         i = int(input())
         g.make_move((i - 1) // 3, (i - 1) % 3)
         if(g.check_win()):
-            print("You is_win!")
+            print("You win!")
             break
         i, j = bestMove(g)
         g.make_move(i, j)
